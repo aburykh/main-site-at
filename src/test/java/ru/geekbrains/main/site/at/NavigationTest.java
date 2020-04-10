0dea5fd42e81d7611bf092d04fab4d6fb888c243
Aleksey Burykh <burykh@azurdrive.ru>
2020-04-10 23:03:32 +0300

    first commit

--- src/test/java/ru/geekbrains/main/site/at/NavigationTest.java
+++ src/test/java/ru/geekbrains/main/site/at/NavigationTest.java
@@ -1,2 +1,73 @@
-public class NavigationTest {
+package ru.geekbrains.main.site.at;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import ru.geekbrains.main.site.at.base.BaseTest;
+
+/**
+ * 1. Перейти на сайт https://geekbrains.ru/courses
+ * 2. Нажать на кнопку Курсы
+ * 3. Проверить что страница Курсы открылась
+ * 4. Повторить для:
+ * - Курсы
+ * - Вебинары
+ * - Форум
+ * - Блог
+ * - Тесты
+ * - Карьера
+ */
+
+public class NavigationTest extends BaseTest {
+
+    @Test
+    void checkNavigation() throws InterruptedException {
+        driver.get("https://geekbrains.ru/career");
+
+        //Курсы
+//        href="/courses"
+        WebElement buttonCourses = driver.findElement(By.cssSelector("[class*=\"main-page-hidden\"] [href=\"/courses\"]"));
+        buttonCourses.click();
+        WebElement headerPageCourses = driver.findElement(By.cssSelector("[class=\"gb-header__title\"]"));
+        Assertions.assertEquals("Курсы", headerPageCourses.getText());
+
+        driver.findElement(By.cssSelector("div button svg[class=\"svg-icon icon-popup-close-button \"]")).click();
+
+        //Вебинары
+//        href="/events"
+        WebElement buttonEvents = driver.findElement(By.cssSelector("[class*=\"main-page-hidden\"] [href=\"/events\"]"));
+        buttonEvents.click();
+        WebElement headerPageEvents = driver.findElement(By.cssSelector("[class=\"gb-header__title\"]"));
+        Assertions.assertEquals("Вебинары", headerPageEvents.getText());
+
+        //Форум
+//        href="/topics"
+        WebElement buttonTopics = driver.findElement(By.cssSelector("[class*=\"main-page-hidden\"] [href=\"/topics\"]"));
+        buttonTopics.click();
+        WebElement headerPageTopics = driver.findElement(By.cssSelector("[class=\"gb-header__title\"]"));
+        Assertions.assertEquals("Форум", headerPageTopics.getText());
+
+        //Блог
+//        href="/posts"
+        WebElement buttonPosts = driver.findElement(By.cssSelector("[class*=\"main-page-hidden\"] [href=\"/posts\"]"));
+        buttonPosts.click();
+        WebElement headerPagePosts = driver.findElement(By.cssSelector("[class=\"gb-header__title\"]"));
+        Assertions.assertEquals("Блог", headerPagePosts.getText());
+
+        //Тесты
+//        href="/tests"
+        WebElement buttonTests = driver.findElement(By.cssSelector("[class*=\"main-page-hidden\"] [href=\"/tests\"]"));
+        buttonTests.click();
+        WebElement headerPageTests = driver.findElement(By.cssSelector("[class=\"gb-header__title\"]"));
+        Assertions.assertEquals("Тесты", headerPageTests.getText());
+
+        //Карьер
+//        href="/career"
+        WebElement buttonCareer = driver.findElement(By.cssSelector("[class*=\"main-page-hidden\"] [href=\"/career\"]"));
+        buttonCareer.click();
+        WebElement headerPageCareer = driver.findElement(By.cssSelector("[class=\"gb-header__title\"]"));
+        Assertions.assertEquals("Карьера", headerPageCareer.getText());
+
+    }
 }
--- src/test/java/ru/geekbrains/main/site/at/base/BaseTest.java
+++ src/test/java/ru/geekbrains/main/site/at/base/BaseTest.java
@@ -1,4 +1,33 @@
+
 package ru.geekbrains.main.site.at.base;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import ru.geekbrains.main.site.at.Header;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+
+        ChromeOptions options  = new ChromeOptions();
+        options.addArguments("--disable-popup-blocking");
+        options.setExperimentalOption("excludeSwitches", Arrays.asList("disable-popup-blocking"));
+        driver = new ChromeDriver(options);
+
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
 }
